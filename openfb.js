f9bd85bb7e06acfe031ebdcae463acd3fba84c37
Tim <tim@live.fi>
2016-09-05 21:07:22 +0300

    fixed access token

--- openfb.js
+++ openfb.js
@@ -238,7 +238,7 @@ var openFB = (function () {
             url;
 
         //params['access_token'] = 'EAAP7TNFRsv8BAJZCrgnSAbxO3ZBVNE7HAOHe5t39z08HxZCRz7yZCXZApCVP70gL8edu6BGIh8muZAB26xBykxD2IiM4LPoqNbrDiu64zOxvcYZCowPbCfQqiegJTmXOgOYKfl5NFfZCyZAyw74xfjDIQRzpT04ZA11wBTnX5ArJVdkAZDZD';
-        tokenStore.fbAccessToken;
+        params['access_token']=tokenStore.fbAccessToken;
 
         url = 'https://graph.facebook.com' + obj.path + '?' + toQueryString(params);
 
